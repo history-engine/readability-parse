defe80fb4b263a5b884c3b941e00b41c51977a08
fengqi <lyf362345@gmail.com>
2023-05-09 18:59:15 +0800

    支持管道输入

--- readability.js
+++ readability.js
@@ -3,17 +3,34 @@
 import {Readability} from "@mozilla/readability";
 import {JSDOM} from "jsdom";
 
-const filepath = process.argv[2]
+const filepath = process.argv[2] || ""
 const encoding = process.argv[3] || "utf-8"
-console.log(filepath, encoding)
 
-if (!filepath) {
-    console.log("filepath empty")
-    process.exit(0)
+if (filepath) {
+    processFile(filepath, encoding);
+} else {
+    let input = '';
+    process.stdin.on('data', (chunk) => {
+        input += chunk;
+    });
+    process.stdin.on('end', () => {
+        processInput(input, encoding);
+    });
 }
 
-JSDOM.fromFile(filepath, encoding && {contentType: `text/html; charset=${encoding}`}).then(dom => {
+function processFile(filepath, encoding) {
+    JSDOM.fromFile(filepath, encoding && {contentType: `text/html; charset=${encoding}`}).then(dom => {
+        processDom(dom)
+    });
+}
+
+function processInput(content, encoding) {
+    let dom = new JSDOM(content, encoding && {contentType: `text/html; charset=${encoding}`})
+    processDom(dom)
+}
+
+function processDom(dom) {
     let article = new Readability(dom.window.document, {debug: false}).parse()
     article['charset'] = dom.window.document.characterSet;
     console.log(JSON.stringify(article, null, 4))
-});
+}
